4e0d312600dc6187579e08584b1b6c66bc1b53b1
zaidyahya <zaid.yahya@mail.mcgill.ca>
2022-04-19 22:01:49 -0400

    Add Spotify Auth

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, session, redirect
+from flask import Flask, session, request, redirect
 from datetime import timedelta
+from urllib.parse import quote
+import requests
+import json
 
 app = Flask(__name__)
 app.config.from_mapping(
@@ -7,17 +10,73 @@ app.config.from_mapping(
     PERMANENT_SESSION_LIFETIME=timedelta(days=7)
 )
 
+# Client Keys
+CLIENT_ID = '0e0d3a9d83594e7fb76911279b6e2aa3'
+CLIENT_SECRET = '9eea43a5790342daab2f152240cd1205'
+
+# Server-side Parameters
+SCOPE = 'user-read-private user-read-email'
+REDIRECT_URI = 'https://cratedigger-klaas.herokuapp.com/api/callback'
+# REDIRECT_URI = 'http://localhost:5000/api/callback'
+STATE = ''
+
+auth_query_parameters = {
+    'response_type': 'code',
+    'redirect_uri': REDIRECT_URI,
+    'scope': SCOPE,
+    'state': STATE,
+    'client_id': CLIENT_ID
+}
+
 @app.before_request
 def before_request():
     session.permanent = True
 
 @app.route("/")
 def hello_world():
-    if 'user' in session:
-        return redirect('/user')
+    return "<p>Hello World!</p>"
+
+    # if 'user' in session:
+    #     return redirect('/user')
+    # else:
+    #     session['user'] = 'Zaid'
+    #     return redirect('/new')
+
+@app.route('/api/authorize')
+def authorize():
+    SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+
+    url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in auth_query_parameters.items()])
+    auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
+    print(auth_url)
+    return redirect(auth_url)
+
+@app.route('/api/callback')
+def callback():
+    print(f'In callback.. {request.args}')
+    # Auth Step 4: Requests refresh and access tokens
+    if 'error' in request.args:
+        print('Request was denied by user')
     else:
-        session['user'] = 'Zaid'
-        return redirect('/new')
+        auth_token = request.args['code']
+        code_payload = {
+            'grant_type': 'authorization_code',
+            'code': str(auth_token),
+            'redirect_uri': REDIRECT_URI,
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+        }
+
+        SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+        post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload)
+
+        # Auth Step 5: Tokens are Returned to Application
+        response_data = json.loads(post_request.text)
+        session['access_token'] = response_data['access_token']
+        session['refresh_token'] = response_data['refresh_token']
+        #print(response_data['access_token'])
+
+    return redirect('/new')
 
 @app.route("/user")
 def user():
